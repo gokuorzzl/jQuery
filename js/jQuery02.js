266076d960e6c6733910324d09e06677afa87375
kim myung sang <gokuorzzl@naver.com>
2016-11-16 19:58:40 +0900

    11.16 수업입니다.

--- js/jQuery02.js
+++ js/jQuery02.js
@@ -8,7 +8,7 @@
     // $(" * ").css("color", "red");  // $=jQuery, "" = 문자열이며, * = 모든것을 의미함
     // $("h1, input").css("color", "red"); // $ = selector, 효과를 줄곳을 선택함.
     // $("#inchon").remove();  // #은 id를 뜻함. id가 inchon인것을 찾도록
-    // $(".myClass").css("background-color", "yellow") // . = 클랫를 찾아줌. 클래스속성이 myClass인걸 찾아줌.
+    // $(".myClass").css("background-color", "yellow") // . = class를 찾아줌. 클래스속성이 myClass인걸 찾아줌.
     // $("[type=button]").css("color", "red"); // "[]" 속성선택자
     // $("div > ul > li[id]").css("color", "red");  // '>' 자식을 표시함.
     // $("div li[id=inchon]").css("color", "red"); // ' ' 공백 = 후손을 표시함.
---
+++ js/jQuery03.js
@@ -0,0 +1,22 @@
+/**
+ * Created by user on 2016-11-16.
+ */
+
+function myFunc() {
+    console.log( $("#apple").text());
+    console.log( $("#pineapple").text());
+    console.log( $("ul > li.myList").text() );
+    console.log( $("from > input").attr("id"));
+    // 동적으로 입력한 값을 가져옴.
+    // text가 아니라, 입력상자안에 값은 val
+    // .attr("id") id라는 속성의 값을 알아냄
+    // console.log( $("ol > li:first").text());
+    // console.log( $("ol > li:last").text());
+    // // :frist >> li 3값중에 첫번째만 가져와줌.
+    // console.log( $("ol > li:nth-child(2)").text());
+    // // nth-child(2) 2번째 자식을 가져와줌
+    // console.log( $("ol > li").text());
+    console.log( $("ol > li.myList" ).text());
+
+    // b = $("ol > li[.myList]").text();
+}
